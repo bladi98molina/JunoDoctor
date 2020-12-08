9863a897cc4f09ec1d625d46fd2a8877c01dd508
Manuel Angel Rivera Martinez <anyel_rmtz26@hotmail.com>
2020-12-08 13:36:48 -0600

    hj

--- Descarga.php
+++ Descarga.php
@@ -7,7 +7,7 @@ if(!empty($fileName) && file_exists($filePath)){
     header("Cache-Control: public");
     header("Content-Description: File Transfer");
     header("Content-Disposition: attachment; filename=$fileName");
-    header("Content-Type: application/zip");
+    header("Content-Type: application/apk");
     header("Content-Transfer-Encoding: binary");
     
     // Read the file
