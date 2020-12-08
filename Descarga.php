8a1a4a2c022862924a39998e80948073498ad4ac
Manuel Angel Rivera Martinez <anyel_rmtz26@hotmail.com>
2020-12-08 13:33:19 -0600

    df

--- Descarga.php
+++ Descarga.php
@@ -1,12 +1,12 @@
 <?php
 
-$fileName = basename('Juno_Doctors.apk');
-$filePath = 'apk/'.$fileName;
+
+$filePath = 'apk/Juno_Doctors.apk';
 if(!empty($fileName) && file_exists($filePath)){
     // Define headers
     header("Cache-Control: public");
     header("Content-Description: File Transfer");
-    header("Content-Disposition: attachment; filename=$fileName");
+    header("Content-Disposition: attachment; filename=Juno_Doctors.apk");
     header("Content-Type: application/zip");
     header("Content-Transfer-Encoding: binary");
     
