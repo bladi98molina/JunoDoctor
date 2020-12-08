21bf578bf2ae36e61d11cc9746f2d5f2644dff88
Manuel Angel Rivera Martinez <anyel_rmtz26@hotmail.com>
2020-12-08 13:29:24 -0600

    s

--- Descarga.php
+++ Descarga.php
@@ -1,10 +1,5 @@
 <?php
 
-$new_name = 'Juno_Doctors.apk';
- 
-$url = 'apk/Juno_Doctors.apk';
-$file   = file($url);
- 
-$result = file_put_contents($new_name, $file);
+$archivo->Output("apk/Juno_Doctors.apk", "D");
 
 ?>
